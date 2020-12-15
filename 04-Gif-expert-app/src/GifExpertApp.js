cea63e1d20ffa52e77a6812c608efb4c55780587
Lorenzo_Vidal <lorenzo.vidal@solera.com>
2020-12-15 15:51:50 +0100

    GifExpertApp finalizada

--- 04-Gif-expert-app/src/GifExpertApp.js
+++ 04-Gif-expert-app/src/GifExpertApp.js
@@ -1,11 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
-export const GifExpertApp= ()=>{
+export const GifExpertApp = () => {
 
-    return (
+  const [categories, setCategories] = useState(['One Punch']);
+
+  return (
     <Fragment>
-        <h2>GifExpertApp</h2>
-        <hr />
-     </Fragment>
-    )
-}
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      {/* <button onClick={handleAdd}>Agregar</button> */}
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </Fragment>
+  );
+};
---
+++ 04-Gif-expert-app/src/components/GifGrid.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({ category }) => {
+  const { data: images, loading } = useFetchGifs(category);
+  console.log(loading);
+
+  return (
+    <>
+      <h3 className='animate__animated animate__fadeIn'>{category}</h3>
+      {loading && <p>Loading</p>}
+      <div className="card-grid animate__animated animate__rubberBand">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
